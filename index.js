71fdaa52d32bca27bf90fca1d4b2d7053418118c
Favour Alfred <alfredfavour76@gmail.com>
2024-11-04 18:20:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -66,3 +66,17 @@ function appendItemToShoppingListEl(item) {
   
   shoppingListEl.append(newEl);
 }
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker.register("/service-worker.js").then(
+      function(registration) {
+        console.log("Service Worker registered with scope:", registration.scope);
+      },
+      function(error) {
+        console.log("Service Worker registration failed:", error);
+      }
+    );
+  });
+}
+
